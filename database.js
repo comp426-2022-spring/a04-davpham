daeb92adb51fcd60d33668c9cd9faead785a7983
David Pham <69774878+davpham@users.noreply.github.com>
2022-04-25 18:48:27 -0400

    test

--- database.js
+++ database.js
@@ -17,11 +17,16 @@ if (row === undefined) {
 // Echo information about what you are doing to the console.
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
-    const sqlInit = `
+    const sqlInitOne = `
+        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
+        INSERT INTO userinfo (username, password) VALUES ('user1','supersecurepass'),('test','another-super-secret-pass');
+    `
+    const sqlInitTwo = `
         CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referer TEXT, useragent TEXT );
     `;
 // Execute SQL commands that we just wrote above.
-    db.exec(sqlInit);
+    db.exec(sqlInitOne)
+    db.exec(sqlInitTwo);
 // Echo information about what we just did to the console.
     //console.log('Your database has been initialized with a new table and two entries containing a username and password.');
 } else {
